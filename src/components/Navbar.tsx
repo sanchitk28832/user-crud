50a0a010daa0d45799bf89f28257825dea58aaa6
sanchitk28832 <153830654+sanchitk28832@users.noreply.github.com>
2024-08-05 18:08:13 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Navbar:React.FC=()=>{
    return(
     <div>
-
     <nav className="p-4 bg-purple-900">
       <Link to="/" className="mr-4 text-white hover:bg-purple-600 p-3 rounded-xl">User List</Link>
       <Link to="/usercards" className="mr-4 text-white hover:bg-purple-600 p-3 rounded-xl">User Cards</Link>
